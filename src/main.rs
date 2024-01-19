5203b114416909628c2fb1380e6c33377c52dfd3
Mohan C M <mohanmanjappa@gmail.com>
2024-01-20 01:45:53 +0530

    JsonParser: fetch message objects randomly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use reqwest::blocking::get;
 use serde_json::Value;
+use rand::Rng;
 
 fn main()  {
 
@@ -9,6 +10,9 @@ fn main()  {
 }
 
 fn fetch_json() -> Result<(), Box<dyn std::error::Error>> {
+
+    // create a new thread-local random number generator (RNG) instance
+    let mut rng = rand::thread_rng();
 
     // add json url
     let json_url = "https://gist.githubusercontent.com/mohancm/60f0f18b88cf4e7cb6644cc1b3329131/raw/f8c7ddc5619fc78fb1ffbe339ff4cd2c59e9e985/inspirations.json";
@@ -20,11 +24,15 @@ fn fetch_json() -> Result<(), Box<dyn std::error::Error>> {
     let json_value: Value = serde_json::from_str(&response)?;
 
     // print out the number of json objects after parsing
-    println!("Number of Objects in the Array: {}", json_value.as_array().unwrap().len());
+    let num_objects = json_value.as_array().unwrap().len();
+    println!("Number of Objects in the Array: {}", num_objects);
+
+    // generate random number between number of json objects
+    let rand_num = rng.gen_range(0..num_objects);
 
     // print out the message of the first object
     println!(
-        "Value of the first object 'message' property: {}", json_value[0]["message"]
+        "Value of the first object 'message' property: {}", json_value[rand_num]["message"]
     );
 
     Ok(())
