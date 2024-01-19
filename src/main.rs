2ad3ef394b0ad8b1244ba4d09eb1fd65ce94e195
Mohan C M <mohanmanjappa@gmail.com>
2024-01-20 02:55:50 +0530

    JsonParser: create separate random fun to return integers (i32)
    Signed-off-by: Mohan C M <mohanmanjappa@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,11 @@ use rand::Rng;
 fn main()  {
 
     // call fetch_json
-    fetch_json();
+    let _ = fetch_json();
     
 }
 
 fn fetch_json() -> Result<(), Box<dyn std::error::Error>> {
-
-    // create a new thread-local random number generator (RNG) instance
-    let mut rng = rand::thread_rng();
 
     // add json url
     let json_url = "https://gist.githubusercontent.com/mohancm/60f0f18b88cf4e7cb6644cc1b3329131/raw/f8c7ddc5619fc78fb1ffbe339ff4cd2c59e9e985/inspirations.json";
@@ -28,7 +25,7 @@ fn fetch_json() -> Result<(), Box<dyn std::error::Error>> {
     println!("Number of Objects in the Array: {}", num_objects);
 
     // generate random number between number of json objects
-    let rand_num = rng.gen_range(0..num_objects);
+    let rand_num = get_random_number(1, num_objects as i32) as usize;
 
     // print out the message of the first object
     println!(
@@ -39,4 +36,11 @@ fn fetch_json() -> Result<(), Box<dyn std::error::Error>> {
 
 }
 
+// returns a random integer btw 'min' and 'max' (inclusive)
+pub fn get_random_number(min: i32, max: i32) -> i32 {
+    assert!(min <= max, "Minimum must be less than or equal to maximum.");
 
+    // create a new thread-local random number generator (RNG) instance
+    let mut rng = rand::thread_rng();
+    rng.gen_range(min..=max)
+}
